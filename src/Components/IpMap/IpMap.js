0eb37ad850b8a17b9b47bb2604898d55cbcf5870
Akinyemi Temiye <akinyemitemiye18@gmail.com>
2022-07-18 09:54:27 +0100

    ip-tracker completed

--- src/Components/IpMap/IpMap.js
+++ src/Components/IpMap/IpMap.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import ChangeView from "./ChangeView";
 import L from "leaflet";
 import iconLocation from "../../assets/icon-location.svg";
 import classes from "./IpMap.module.css";
 // import "leaflet/dist/leaflet.css";
 import "./leaflet.css";
-import ChangeView from "./ChangeView";
 
 function GetIcon(_iconSize) {
   return L.icon({
@@ -25,16 +25,14 @@ const IpMap = ({ ipData }) => {
 
   return (
     <section className={classes.mapContainer}>
-      {center && (
-        <MapContainer center={center} zoom={zoom} scrollWheelZoom={true}>
-          <ChangeView center={center} zoom={zoom} />
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[lat, lng]} icon={GetIcon(35)}></Marker>
-        </MapContainer>
-      )}
+      <MapContainer center={center} zoom={zoom} scrollWheelZoom={true}>
+        <ChangeView center={center} zoom={zoom} scrollWheelZoom={true} />
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[lat, lng]} icon={GetIcon(35)}></Marker>
+      </MapContainer>
     </section>
   );
 };
